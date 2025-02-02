a4aa2cdc01fae77481fc7de508ab291ac801defe
hellaz <50288964+hellaz@users.noreply.github.com>
2025-02-02 13:15:26 +0200

    Update class-hsz-shortcode.php

--- includes/class-hsz-shortcode.php
+++ includes/class-hsz-shortcode.php
@@ -1,1 +1,18 @@
 // Shortcode integration
+namespace HSZ;
+
+class Shortcode {
+    public function __construct() {
+        add_shortcode('hsz_metadata', [$this, 'render_shortcode']);
+    }
+
+    public function render_shortcode($atts) {
+        $atts = shortcode_atts(['url' => ''], $atts);
+        $url = esc_url($atts['url']);
+        $metadata = Metadata::extract_metadata($url);
+
+        ob_start();
+        include HSZ_PLUGIN_PATH . 'templates/metadata-template.php';
+        return ob_get_clean();
+    }
+}

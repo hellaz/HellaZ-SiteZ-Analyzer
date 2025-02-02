13edef51e3077492e42f32268cb43ef5165950ee
hellaz <50288964+hellaz@users.noreply.github.com>
2025-02-02 13:14:39 +0200

    Update class-hsz-rss.php

--- includes/class-hsz-rss.php
+++ includes/class-hsz-rss.php
@@ -1,1 +1,20 @@
 // RSS feed handling logic
+namespace HSZ;
+
+class RSS {
+    public function __construct() {
+        add_action('init', [$this, 'detect_rss_feeds']);
+    }
+
+    public function detect_rss_feeds($html) {
+        $pattern = '/<link[^>]+type="application\/rss\+xml"[^>]*>/i';
+        preg_match_all($pattern, $html, $matches);
+        $feeds = [];
+        foreach ($matches[0] as $match) {
+            if (preg_match('/href="([^"]+)"/i', $match, $href)) {
+                $feeds[] = $href[1];
+            }
+        }
+        return $feeds;
+    }
+}

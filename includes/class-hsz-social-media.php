c6ce18660b210d8030d831efe1c5a063a2bdb825
hellaz <50288964+hellaz@users.noreply.github.com>
2025-02-02 19:17:54 +0200

    Update class-hsz-social-media.php

--- includes/class-hsz-social-media.php
+++ includes/class-hsz-social-media.php
@@ -4,17 +4,19 @@ namespace HSZ;
 class SocialMedia {
     private $platforms = ['facebook', 'twitter', 'instagram', 'linkedin', 'youtube'];
 
-    public function detect_links($html) {
-        $links = [];
-        foreach ($this->platforms as $platform) {
-            $pattern = "/https?:\/\/(www\.)?$platform\.com\/[^\s\"']+/i";
-            preg_match_all($pattern, $html, $matches);
-            if (!empty($matches[0])) {
-                $links[$platform] = array_unique($matches[0]);
-            }
-        }
-        return $links;
-    }
+ //* detects all <a> tags with href attributes, which is broader than necessary.   
+ //   public function detect_links($html) {
+ //       $links = [];
+ //       foreach ($this->platforms as $platform) {
+ //           $pattern = "/https?:\/\/(www\.)?$platform\.com\/[^\s\"']+/i";
+ //           preg_match_all($pattern, $html, $matches);
+ //           if (!empty($matches[0])) {
+ //               $links[$platform] = array_unique($matches[0]);
+ //           }
+ //       }
+ //       return $links;
+ //   }
+ 
     public function detect_social_media_links($html) {
             $platforms = [
                 'facebook' => '/facebook\.com/i',

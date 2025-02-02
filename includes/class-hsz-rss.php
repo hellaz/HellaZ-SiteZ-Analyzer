8793e4042019e0f18eb3463d90db2b22e2551a6e
hellaz <50288964+hellaz@users.noreply.github.com>
2025-02-02 19:19:21 +0200

    Update class-hsz-rss.php

--- includes/class-hsz-rss.php
+++ includes/class-hsz-rss.php
@@ -18,16 +18,17 @@ class RSS {
         return $feeds;
     }
 
-    public function detect_feeds($html) {
-        $pattern = '/<link[^>]+type="application\/(rss|atom)\+xml"[^>]*>/i';
-        preg_match_all($pattern, $html, $matches);
-        $feeds = [];
-        foreach ($matches[0] as $match) {
-            if (preg_match('/href="([^"]+)"/i', $match, $href)) {
-                $feeds[] = $href[1];
-            }
-        }
-        return $feeds;
-    }
+// Redundant based on the above    
+//    public function detect_feeds($html) {
+//        $pattern = '/<link[^>]+type="application\/(rss|atom)\+xml"[^>]*>/i';
+//        preg_match_all($pattern, $html, $matches);
+//        $feeds = [];
+//        foreach ($matches[0] as $match) {
+//            if (preg_match('/href="([^"]+)"/i', $match, $href)) {
+//                $feeds[] = $href[1];
+//            }
+//        }
+//        return $feeds;
+//    }
 
 }

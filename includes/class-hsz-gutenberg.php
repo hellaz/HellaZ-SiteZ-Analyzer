69a2ec5b1d6f40b7f7d8f770751fbe0ab1d311e1
hellaz <50288964+hellaz@users.noreply.github.com>
2025-02-02 17:19:58 +0200

    Update class-hsz-gutenberg.php

--- includes/class-hsz-gutenberg.php
+++ includes/class-hsz-gutenberg.php
@@ -29,17 +29,26 @@ class Gutenberg {
         ]);
     }
 
-    public function render_block($attributes) {
-        $url = isset($attributes['url']) ? esc_url($attributes['url']) : '';
-        if (empty($url)) {
-            return '<p>' . __('Please provide a valid URL.', 'hellaz-sitez-analyzer') . '</p>';
-        }
-
-        // Extract metadata
-        $metadata = (new Metadata())->extract_metadata($url);
-
-        ob_start();
-        include HSZ_PLUGIN_PATH . 'templates/metadata-template.php';
-        return ob_get_clean();
+    public function register_block() {
+        // Register block script
+        wp_register_script(
+            'hsz-gutenberg-block',
+            HSZ_PLUGIN_URL . 'assets/js/scripts.js', // Ensure this path is correct
+            ['wp-blocks', 'wp-element', 'wp-editor'],
+            '1.0.0',
+            true
+        );
+    
+        // Register the block
+        register_block_type('hsz/metadata-block', [
+            'attributes' => [
+                'url' => [
+                    'type' => 'string',
+                    'default' => '',
+                ],
+            ],
+            'editor_script' => 'hsz-gutenberg-block',
+            'render_callback' => [$this, 'render_block'],
+        ]);
     }
 }

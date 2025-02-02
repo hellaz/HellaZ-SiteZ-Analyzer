d29cac318457da78a44df898035dbf43b25021f0
hellaz <50288964+hellaz@users.noreply.github.com>
2025-02-02 13:04:26 +0200

    Update metadata-template.php

--- templates/metadata-template.php
+++ templates/metadata-template.php
@@ -1,1 +1,6 @@
 // Template for metadata display
+// metadata-template.php
+<div class="hsz-metadata">
+    <h3><?php echo esc_html($metadata['title']); ?></h3>
+    <p><?php echo esc_html($metadata['description']); ?></p>
+</div>

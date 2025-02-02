310063971325d8650783a9003f0e46b5d26feb52
hellaz <50288964+hellaz@users.noreply.github.com>
2025-02-02 17:28:14 +0200

    Update class-hsz-gutenberg.php

--- includes/class-hsz-gutenberg.php
+++ includes/class-hsz-gutenberg.php
@@ -7,29 +7,6 @@ class Gutenberg {
     }
 
     public function register_block() {
-        // Register block script
-        wp_register_script(
-            'hsz-gutenberg-block',
-            HSZ_PLUGIN_URL . 'assets/js/scripts.js',
-            ['wp-blocks', 'wp-element', 'wp-editor'],
-            '1.0.0',
-            true
-        );
-
-        // Register the block
-        register_block_type('hsz/metadata-block', [
-            'attributes' => [
-                'url' => [
-                    'type' => 'string',
-                    'default' => '',
-                ],
-            ],
-            'editor_script' => 'hsz-gutenberg-block',
-            'render_callback' => [$this, 'render_block'],
-        ]);
-    }
-
-    public function register_block() {
         error_log('Registering Gutenberg block...'); // Debugging
 
         // Register block script

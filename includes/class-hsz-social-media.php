19f0cf2747c18ecc4c76825e8c821691b3b1ac02
hellaz <50288964+hellaz@users.noreply.github.com>
2025-02-02 13:02:55 +0200

    Update class-hsz-social-media.php

--- includes/class-hsz-social-media.php
+++ includes/class-hsz-social-media.php
@@ -1,1 +1,18 @@
 // Social media detection logic
+namespace HSZ;
+
+class SocialMedia {
+    private $platforms = ['facebook', 'twitter', 'instagram', 'linkedin', 'youtube'];
+
+    public function detect_links($html) {
+        $links = [];
+        foreach ($this->platforms as $platform) {
+            $pattern = "/https?:\/\/(www\.)?$platform\.com\/[^\s\"']+/i";
+            preg_match_all($pattern, $html, $matches);
+            if (!empty($matches[0])) {
+                $links[$platform] = array_unique($matches[0]);
+            }
+        }
+        return $links;
+    }
+}

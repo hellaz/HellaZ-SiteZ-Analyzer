45c9fc028bf24740cc0db084e831b5bbbbf94153
hellaz <50288964+hellaz@users.noreply.github.com>
2025-02-02 13:03:48 +0200

    Update class-hsz-cache.php

--- includes/class-hsz-cache.php
+++ includes/class-hsz-cache.php
@@ -1,1 +1,19 @@
 // Caching system (Transient API)
+namespace HSZ;
+
+class Cache {
+    public function get_cached_data($url) {
+        $cache_key = 'hsz_' . md5($url);
+        $data = get_transient($cache_key);
+        if ($data === false) {
+            $data = $this->fetch_data($url);
+            set_transient($cache_key, $data, DAY_IN_SECONDS);
+        }
+        return $data;
+    }
+
+    private function fetch_data($url) {
+        // Fetch and process data here
+        return [];
+    }
+}

14ccdb58845c96f5dfb0a02e84f3b2539ac4f3fa
hellaz <50288964+hellaz@users.noreply.github.com>
2025-02-03 22:44:50 +0200

    Revert "Update class-hsz-settings.php"
    This reverts commit f0a446bb547f4d2b45045104883043976ae49967.

--- includes/class-hsz-settings.php
+++ includes/class-hsz-settings.php
@@ -17,102 +17,139 @@ class Settings {
         );
     }
 
-    public function register_settings() {
-        // Register API key settings
-        register_setting('hsz-settings-group', 'hsz_virustotal_api_key');
-        register_setting('hsz-settings-group', 'hsz_urlscan_api_key');
-        register_setting('hsz-settings-group', 'hsz_builtwith_api_key');
-
-        // Register disclaimer settings
-        register_setting('hsz-settings-group', 'hsz_enable_disclaimer');
-        register_setting('hsz-settings-group', 'hsz_disclaimer_message');
-
-        // Register fallback image setting
-        register_setting('hsz-settings-group', 'hsz_fallback_image');
-
-        // Register link target setting
-        register_setting('hsz-settings-group', 'hsz_link_target');
-    }
-
     public function render_settings_page() {
         ?>
         <div class="wrap">
             <h1><?php _e('HellaZ SiteZ Analyzer Settings', 'hellaz-sitez-analyzer'); ?></h1>
             <form method="post" action="options.php">
-                <?php settings_fields('hsz-settings-group'); ?>
-                <?php do_settings_sections('hsz-settings-group'); ?>
-
-                <table class="form-table">
-                    <!-- VirusTotal API Key -->
-                    <tr>
-                        <th scope="row"><label for="hsz_virustotal_api_key"><?php _e('VirusTotal API Key', 'hellaz-sitez-analyzer'); ?></label></th>
-                        <td>
-                            <input type="text" id="hsz_virustotal_api_key" name="hsz_virustotal_api_key" value="<?php echo esc_attr(get_option('hsz_virustotal_api_key')); ?>" class="regular-text">
-                            <p class="description"><?php _e('Enter your VirusTotal API key.', 'hellaz-sitez-analyzer'); ?></p>
-                        </td>
-                    </tr>
-
-                    <!-- URLScan.io API Key -->
-                    <tr>
-                        <th scope="row"><label for="hsz_urlscan_api_key"><?php _e('URLScan.io API Key', 'hellaz-sitez-analyzer'); ?></label></th>
-                        <td>
-                            <input type="text" id="hsz_urlscan_api_key" name="hsz_urlscan_api_key" value="<?php echo esc_attr(get_option('hsz_urlscan_api_key')); ?>" class="regular-text">
-                            <p class="description"><?php _e('Enter your URLScan.io API key.', 'hellaz-sitez-analyzer'); ?></p>
-                        </td>
-                    </tr>
-
-                    <!-- BuiltWith API Key -->
-                    <tr>
-                        <th scope="row"><label for="hsz_builtwith_api_key"><?php _e('BuiltWith API Key', 'hellaz-sitez-analyzer'); ?></label></th>
-                        <td>
-                            <input type="text" id="hsz_builtwith_api_key" name="hsz_builtwith_api_key" value="<?php echo esc_attr(get_option('hsz_builtwith_api_key')); ?>" class="regular-text">
-                            <p class="description"><?php _e('Enter your BuiltWith API key.', 'hellaz-sitez-analyzer'); ?></p>
-                        </td>
-                    </tr>
-
-                    <!-- Fallback Image -->
-                    <tr>
-                        <th scope="row"><label for="hsz_fallback_image"><?php _e('Fallback Image URL', 'hellaz-sitez-analyzer'); ?></label></th>
-                        <td>
-                            <input type="text" id="hsz_fallback_image" name="hsz_fallback_image" value="<?php echo esc_attr(get_option('hsz_fallback_image')); ?>" class="regular-text">
-                            <p class="description"><?php _e('Enter the URL of the fallback image to use when no favicon is found.', 'hellaz-sitez-analyzer'); ?></p>
-                        </td>
-                    </tr>
-
-                    <!-- Enable Disclaimer -->
-                    <tr>
-                        <th scope="row"><label for="hsz_enable_disclaimer"><?php _e('Enable Disclaimer', 'hellaz-sitez-analyzer'); ?></label></th>
-                        <td>
-                            <input type="checkbox" id="hsz_enable_disclaimer" name="hsz_enable_disclaimer" value="1" <?php checked(get_option('hsz_enable_disclaimer'), 1); ?>>
-                            <p class="description"><?php _e('Check to enable the disclaimer message.', 'hellaz-sitez-analyzer'); ?></p>
-                        </td>
-                    </tr>
-
-                    <!-- Disclaimer Message -->
-                    <tr>
-                        <th scope="row"><label for="hsz_disclaimer_message"><?php _e('Disclaimer Message', 'hellaz-sitez-analyzer'); ?></label></th>
-                        <td>
-                            <textarea id="hsz_disclaimer_message" name="hsz_disclaimer_message" rows="5" class="large-text"><?php echo esc_textarea(get_option('hsz_disclaimer_message')); ?></textarea>
-                            <p class="description"><?php _e('Enter the disclaimer message to display.', 'hellaz-sitez-analyzer'); ?></p>
-                        </td>
-                    </tr>
-
-                    <!-- Link Target -->
-                    <tr>
-                        <th scope="row"><label for="hsz_link_target"><?php _e('Link Target', 'hellaz-sitez-analyzer'); ?></label></th>
-                        <td>
-                            <select id="hsz_link_target" name="hsz_link_target">
-                                <option value="_blank" <?php selected(get_option('hsz_link_target'), '_blank'); ?>><?php _e('Open in New Window', 'hellaz-sitez-analyzer'); ?></option>
-                                <option value="_top" <?php selected(get_option('hsz_link_target'), '_top'); ?>><?php _e('Open in Same Window', 'hellaz-sitez-analyzer'); ?></option>
-                            </select>
-                            <p class="description"><?php _e('Choose how links should open.', 'hellaz-sitez-analyzer'); ?></p>
-                        </td>
-                    </tr>
-                </table>
-
-                <?php submit_button(); ?>
+                <?php
+                settings_fields('hsz-settings');
+                do_settings_sections('hsz-settings');
+                submit_button();
+                ?>
             </form>
         </div>
         <?php
+    }
+
+    public function register_settings() {
+        add_settings_section(
+            'hsz_general_settings',
+            __('General Settings', 'hellaz-sitez-analyzer'),
+            [$this, 'render_general_settings_description'],
+            'hsz-settings'
+        );
+
+        // Fallback Image
+        add_settings_field(
+            'hsz_fallback_image',
+            __('Fallback Image', 'hellaz-sitez-analyzer'),
+            [$this, 'render_fallback_image_field'],
+            'hsz-settings',
+            'hsz_general_settings'
+        );
+        register_setting('hsz-settings', 'hsz_fallback_image');
+
+        // Disclaimer Enable/Disable
+        add_settings_field(
+            'hsz_enable_disclaimer',
+            __('Enable Disclaimer', 'hellaz-sitez-analyzer'),
+            [$this, 'render_enable_disclaimer_field'],
+            'hsz-settings',
+            'hsz_general_settings'
+        );
+        register_setting('hsz-settings', 'hsz_enable_disclaimer', [
+            'type' => 'boolean',
+            'default' => false,
+        ]);
+
+        // Disclaimer Message
+        add_settings_field(
+            'hsz_disclaimer_message',
+            __('Disclaimer Message', 'hellaz-sitez-analyzer'),
+            [$this, 'render_disclaimer_message_field'],
+            'hsz-settings',
+            'hsz_general_settings'
+        );
+        register_setting('hsz-settings', 'hsz_disclaimer_message');
+
+        // External API Settings
+        add_settings_section(
+            'hsz_api_settings',
+            __('External API Settings', 'hellaz-sitez-analyzer'),
+            [$this, 'render_api_settings_description'],
+            'hsz-settings'
+        );
+
+        // VirusTotal API Key
+        add_settings_field(
+            'hsz_virustotal_api_key',
+            __('VirusTotal API Key', 'hellaz-sitez-analyzer'),
+            [$this, 'render_virustotal_api_key_field'],
+            'hsz-settings',
+            'hsz_api_settings'
+        );
+        register_setting('hsz-settings', 'hsz_virustotal_api_key');
+
+        // URLScan.io API Key
+        add_settings_field(
+            'hsz_urlscan_api_key',
+            __('URLScan.io API Key', 'hellaz-sitez-analyzer'),
+            [$this, 'render_urlscan_api_key_field'],
+            'hsz-settings',
+            'hsz_api_settings'
+        );
+        register_setting('hsz-settings', 'hsz_urlscan_api_key');
+
+        // BuiltWith API Key
+        add_settings_field(
+            'hsz_builtwith_api_key',
+            __('BuiltWith API Key', 'hellaz-sitez-analyzer'),
+            [$this, 'render_builtwith_api_key_field'],
+            'hsz-settings',
+            'hsz_api_settings'
+        );
+        register_setting('hsz-settings', 'hsz_builtwith_api_key');
+    }
+
+    public function render_general_settings_description() {
+        echo '<p>' . __('Configure general settings for the HellaZ SiteZ Analyzer plugin.', 'hellaz-sitez-analyzer') . '</p>';
+    }
+
+    public function render_fallback_image_field() {
+        $fallback_image = get_option('hsz_fallback_image', '');
+        echo '<input type="text" name="hsz_fallback_image" value="' . esc_attr($fallback_image) . '" class="regular-text">';
+    }
+
+    public function render_enable_disclaimer_field() {
+        $enable_disclaimer = get_option('hsz_enable_disclaimer', false);
+        echo '<input type="checkbox" name="hsz_enable_disclaimer" value="1"' . checked(1, $enable_disclaimer, false) . '>';
+    }
+
+    public function render_disclaimer_message_field() {
+        $disclaimer_message = get_option('hsz_disclaimer_message', __('This is a default disclaimer message.', 'hellaz-sitez-analyzer'));
+        echo '<textarea name="hsz_disclaimer_message" rows="5" class="large-text">' . esc_textarea($disclaimer_message) . '</textarea>';
+    }
+
+    public function render_api_settings_description() {
+        echo '<p>' . __('Configure API keys for external services. Leave blank if you do not wish to use these services.', 'hellaz-sitez-analyzer') . '</p>';
+    }
+
+    public function render_virustotal_api_key_field() {
+        $api_key = get_option('hsz_virustotal_api_key', '');
+        echo '<input type="text" name="hsz_virustotal_api_key" value="' . esc_attr($api_key) . '" class="regular-text">';
+        echo '<p class="description">' . __('Get your API key from <a href="https://www.virustotal.com/" target="_blank">VirusTotal</a>.', 'hellaz-sitez-analyzer') . '</p>';
+    }
+
+    public function render_urlscan_api_key_field() {
+        $api_key = get_option('hsz_urlscan_api_key', '');
+        echo '<input type="text" name="hsz_urlscan_api_key" value="' . esc_attr($api_key) . '" class="regular-text">';
+        echo '<p class="description">' . __('Get your API key from <a href="https://urlscan.io/" target="_blank">URLScan.io</a>.', 'hellaz-sitez-analyzer') . '</p>';
+    }
+
+    public function render_builtwith_api_key_field() {
+        $api_key = get_option('hsz_builtwith_api_key', '');
+        echo '<input type="text" name="hsz_builtwith_api_key" value="' . esc_attr($api_key) . '" class="regular-text">';
+        echo '<p class="description">' . __('Get your API key from <a href="https://builtwith.com/" target="_blank">BuiltWith</a>.', 'hellaz-sitez-analyzer') . '</p>';
     }
 }

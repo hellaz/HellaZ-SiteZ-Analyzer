65da5cd018b2bffadef2444a08e80f676e870dc5
hellaz <50288964+hellaz@users.noreply.github.com>
2025-02-06 23:55:38 +0200

    Update class-hsz-utils.php

--- includes/class-hsz-utils.php
+++ includes/class-hsz-utils.php
@@ -8,12 +8,17 @@ class Utils {
      * @param string $message The error message to log.
      */
     public static function log_admin_notice($message) {
+        // Validate the message
+        if (!is_string($message) || empty(trim($message))) {
+            return;
+        }
+
         // Add an admin notice
         add_action('admin_notices', function () use ($message) {
             echo '<div class="notice notice-error"><p>' . esc_html($message) . '</p></div>';
         });
 
         // Log the error to the debug log
-        error_log('[HellaZ SiteZ Analyzer] ' . $message);
+        error_log('[HellaZ SiteZ Analyzer] Error: ' . $message);
     }
 }

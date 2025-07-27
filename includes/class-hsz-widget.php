7647beed0c7a035ceb1b20f63261a2688f592308
hellaz <50288964+hellaz@users.noreply.github.com>
2025-07-27 21:35:15 +0300

    Static Registration Method to Widget Class

--- includes/class-hsz-widget.php
+++ includes/class-hsz-widget.php
@@ -1,13 +1,18 @@
 <?php
 namespace HSZ;
 
-if (!defined('ABSPATH')) {
-    exit;
-}
+use WP_Widget;
 
-class Widget extends \WP_Widget {
-    
-    public function __construct() {
+if (!defined('ABSPATH')) exit;
+
+class Widget extends WP_Widget
+{
+    public static function register_widget() {
+        register_widget(__CLASS__);
+    }
+
+    public function __construct()
+    {
         parent::__construct(
             'hsz_widget',
             __('HellaZ SiteZ Analyzer', 'hellaz-sitez-analyzer'),
@@ -15,13 +20,13 @@ class Widget extends \WP_Widget {
         );
     }
 
-    public function widget($args, $instance) {
+    public function widget($args, $instance)
+    {
         echo $args['before_widget'];
 
         $title = !empty($instance['title']) ? apply_filters('widget_title', $instance['title']) : '';
         $url = !empty($instance['url']) ? esc_url($instance['url']) : '';
 
-        // Validate the URL
         if (!filter_var($url, FILTER_VALIDATE_URL)) {
             echo '<p>' . __('Please provide a valid URL.', 'hellaz-sitez-analyzer') . '</p>';
             echo $args['after_widget'];
@@ -32,7 +37,6 @@ class Widget extends \WP_Widget {
             echo $args['before_title'] . $title . $args['after_title'];
         }
 
-        // Check if the template file exists
         $template_path = HSZ_PLUGIN_PATH . 'templates/metadata-template.php';
         if (!file_exists($template_path)) {
             echo '<p>' . __('Template file is missing.', 'hellaz-sitez-analyzer') . '</p>';
@@ -41,17 +45,14 @@ class Widget extends \WP_Widget {
         }
 
         try {
-            // Extract metadata using the Metadata class
             $metadata = (new Metadata())->extract_metadata($url);
 
-            // Check for errors
             if (isset($metadata['error'])) {
                 echo '<p>' . esc_html($metadata['error']) . '</p>';
                 echo $args['after_widget'];
                 return;
             }
 
-            // Start output buffering and include the template file
             ob_start();
             include $template_path;
             echo ob_get_clean();
@@ -63,7 +64,8 @@ class Widget extends \WP_Widget {
         echo $args['after_widget'];
     }
 
-    public function form($instance) {
+    public function form($instance)
+    {
         $title = !empty($instance['title']) ? $instance['title'] : '';
         $url = !empty($instance['url']) ? $instance['url'] : '';
         ?>
@@ -78,7 +80,8 @@ class Widget extends \WP_Widget {
         <?php
     }
 
-    public function update($new_instance, $old_instance) {
+    public function update($new_instance, $old_instance)
+    {
         $instance = [];
         $instance['title'] = !empty($new_instance['title']) ? sanitize_text_field($new_instance['title']) : '';
         $instance['url'] = !empty($new_instance['url']) ? esc_url_raw($new_instance['url']) : '';
